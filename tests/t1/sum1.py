903d60dffe952229bc4d60f2dce8e1d7bb4d7cc5
Jonathon <ben0007@jmss.vic.edu.au>
2016-02-29 15:07:46 +1100

    Index error code

--- tests/t1/sum1.py
+++ tests/t1/sum1.py
@@ -1,1 +1,16 @@
 # write a program that reads in 10 numbers, then prints the sum of those
+
+a = int(input("Input number 1 "))
+b = int(input("Input number 2 "))
+c = int(input("Input number 3 "))
+d = int(input("Input number 4 "))
+e = int(input("Input number 5 "))
+f = int(input("Input number 6 "))
+g = int(input("Input number 7 "))
+h = int(input("Input number 8 "))
+i = int(input("Input number 9 "))
+j = int(input("Input number 10 "))
+
+print("The answer is ", a+b+c+d+e+f+g+h+i+j)
+
+
